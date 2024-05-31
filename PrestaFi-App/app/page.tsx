a47d12c42ac98f9ef4a15db668e83d8892b0b22b
Julio M Cruz <julio.cruz@eb-ms.net>
2024-05-31 01:10:04 -0500

    Update page.tsx

--- PrestaFi-App/app/page.tsx
+++ PrestaFi-App/app/page.tsx
@@ -19,30 +19,30 @@ export default function Home() {
 
   return (
     <>
-    <div className="px-10 py-20">
-      <div className="absolute -z-50 top-0 mx-auto w-1/2 h-1/2 bg-primary/50 blur-[256px] opacity-75 rounded-full -z-1" />
-      <div className="absolute -z-50 top-0 left-0 w-1/2 h-1/2 bg-green-300/70 blur-[256px] opacity-45 rounded-full -z-1" />
-      <div className="space-y-20">
-        <div className="flex flex-col items-center w-full space-y-4">
+      <div className="px-10 py-20">
+        <div className="absolute -z-50 top-0 mx-auto w-1/2 h-1/2 bg-primary/50 blur-[256px] opacity-75 rounded-full -z-1" />
+        <div className="absolute -z-50 top-0 left-0 w-1/2 h-1/2 bg-green-300/70 blur-[256px] opacity-45 rounded-full -z-1" />
+        <div className="space-y-20">
+          <div className="flex flex-col items-center w-full space-y-4">
 
-          <HeaderComponent />     
+            <HeaderComponent />
 
-          <HeroComponent />
+            <HeroComponent />
 
-          {/* <CallToActionComponent /> */}
+            {/* <CallToActionComponent /> */}
 
-          <HowWorksComponent />
+            <HowWorksComponent />
 
-          <BenefitsComponent />
+            <BenefitsComponent />
 
-          <main className="flex flex-col items-center">
-          </main>
+            <main className="flex flex-col items-center">
+            </main>
 
 
+          </div>
         </div>
       </div>
-    </div>
-          <FooterSection />
-          </>
+      <FooterSection />
+    </>
   );
 }
